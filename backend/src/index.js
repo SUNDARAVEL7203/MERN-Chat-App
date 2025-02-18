d6eff463128c72374a4bad4adabc5e6cfffc74d8
SUNDARAVEL7203 <sundarvel.kasi@gmail.com>
2025-02-18 22:56:38 +0530

    chatapp

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,39 +1,42 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import { connectDB } from './lib/db.js';
-import cookieParser from "cookie-parser"
-import cors from 'cors'
-import path from 'path'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js"
-import { app, server } from './lib/socket.js';
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
+dotenv.config();
 
-
-dotenv.config()
-const PORT = process.env.PORT 
-const _dirname = path.resolve()
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.use(cors({
-    origin: 'http://localhost:5173',
+app.use(
+  cors({
+    origin: "http://localhost:5173",
     credentials: true,
-}))// Allow the cookies to be sent with the request
+  })
+);
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes)
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-  }
-server.listen(5000, () => {
-    console.log('Server is running on port 5000');
-    connectDB()
-})
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
